291733d0994e3b75bec6a2fdd668e0b0f93da668
wojtek <wojciech.jargielo@gmail.com>
2018-03-20 23:15:49 +0100

    no message

--- Projekty/Projekt_Lab3/zadanie4.c
+++ Projekty/Projekt_Lab3/zadanie4.c
@@ -1,28 +1,23 @@
-/*
- * Data:                2009-02-27
- * Autor:               Jakub Gasior <quebes@mars.iti.pk.edu.pl>
- * Kompilacja:          $ gcc udp.c -o udp
- * Uruchamianie:        $ ./udp <adres IP lub nazwa domenowa> <numer portu>
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#ifndef __USE_BSD
+#define __USE_BSD
+#endif
 #include <netinet/ip.h>
-
+#define __FAVOR_BSD
 #include <netinet/udp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <string.h>
-#include <error.h>
+// #include <error.h>
 #include "checksum.h"
 
+#define SOURCE_PORT 5555
+#define SOURCE_ADDRESS "192.168.0.250"
 
-
-/* Struktura pseudo-naglowka (do obliczania sumy kontrolnej naglowka UDP): */
 struct phdr {
     struct in_addr ip_src, ip_dst;
     unsigned char unused;
@@ -33,52 +28,30 @@ struct phdr {
 
 int main(int argc, char** argv) {
 
-    int                     sockfd; /* Deskryptor gniazda. */
-    int                     socket_option; /* Do ustawiania opcji gniazda. */
-    int                     retval; /* Wartosc zwracana przez funkcje. */
+    int     sockfd;
+    int     offset_option;
+    int     retval;
 
-    /* Struktura zawierajaca wskazowki dla funkcji getaddrinfo(): */
-    struct addrinfo         hints;
+    struct          addrinfo        hints;
+    struct          addrinfo        *rp, *result;
 
-    /*
-     * Wskaznik na liste zwracana przez getaddrinfo() oraz wskaznik uzywany do
-     * poruszania sie po elementach listy:
-     */
-    struct addrinfo         *rp, *result;
+    unsigned short  checksum;
+    unsigned char   datagram[sizeof(struct udphdr) + sizeof(struct phdr)] = {0};
 
-    /* Zmienna wykorzystywana do obliczenia sumy kontrolnej: */
-    unsigned short          checksum;
+    struct udphdr   *udp_header     = (struct udphdr *)(datagram);
+    struct phdr     *pseudo_header  = (struct phdr *) (datagram + sizeof(struct udphdr));
 
-    /* Bufor na naglowek IP, naglowek UDP oraz pseudo-naglowek: */
-    unsigned char           datagram[sizeof(struct ip) + sizeof(struct udphdr)
-                                     + sizeof(struct phdr)] = {0};
 
-    /* Wskaznik na naglowek IP (w buforze okreslonym przez 'datagram'): */
-    struct ip               *ip_header      = (struct ip *)datagram;
-
-    /* Wskaznik na naglowek UDP (w buforze okreslonym przez 'datagram'): */
-    struct udphdr           *udp_header     = (struct udphdr *)
-            (datagram + sizeof(struct ip));
-    /* Wskaznik na pseudo-naglowek (w buforze okreslonym przez 'datagram'): */
-    struct phdr             *pseudo_header  = (struct phdr *)
-            (datagram + sizeof(struct ip)
-             + sizeof(struct udphdr));
-    /* SPrawdzenie argumentow wywolania: */
     if (argc != 3) {
-        fprintf(
-            stderr,
-            "Invocation: %s <HOSTNAME OR IP ADDRESS> <PORT>\n",
-            argv[0]
-        );
-
+        fprintf(stderr,"Invocation: %s <HOSTNAME OR IPv6 ADDRESS> <PORT>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    /* Wskazowki dla getaddrinfo(): */
+
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family         =       AF_INET6; /* Domena komunikacyjna (IPv4). */
-    hints.ai_socktype       =       SOCK_RAW; /* Typ gniazda. */
-    hints.ai_protocol       =       IPPROTO_UDP; /* Protokol. */
+    hints.ai_family         =       AF_INET6;
+    hints.ai_socktype       =       SOCK_RAW;
+    hints.ai_protocol       =       IPPROTO_UDP;
 
 
     retval = getaddrinfo(argv[1], NULL, &hints, &result);
@@ -87,13 +60,13 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    /* Opcja okreslona w wywolaniu setsockopt() zostanie wlaczona: */
-    socket_option = 1;
 
-    /* Przechodzimy kolejno przez elementy listy: */
+    //
+
+    offset_option = 6;
+
     for (rp = result; rp != NULL; rp = rp->ai_next) {
 
-        /* Utworzenie gniazda dla protokolu UDP: */
         sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
         if (sockfd == -1) {
             perror("socket()");
@@ -101,56 +74,43 @@ int main(int argc, char** argv) {
         }
 
         /* Ustawienie opcji IP_HDRINCL: */
-        int offset = 6;
         retval = setsockopt(
                      sockfd,
                      IPPROTO_IPV6, IPV6_CHECKSUM,
-                     &offset, sizeof(int)
+                     &offset_option, sizeof(int)
                  );
 
         if (retval == -1) {
             perror("setsockopt()");
             exit(EXIT_FAILURE);
         } else {
-            /* Jezeli gniazdo zostalo poprawnie utworzone i
-             * opcja IP_HDRINCL ustawiona: */
+            printf("created\n");
             break;
         }
     }
 
-    /* Jezeli lista jest pusta (nie utworzono gniazda): */
     if (rp == NULL) {
         fprintf(stderr, "Client failure: could not create socket.\n");
         exit(EXIT_FAILURE);
     }
 
-
+    //
     /*********************************/
     /* Wypelnienie pol naglowka UDP: */
     /*********************************/
 
 
-    /* Port docelowy (z argumentu wywolania): */
-    udp_header->uh_dport            =       htons(atoi(argv[2]));
 
-    /* Rozmiar naglowka UDP i danych. W tym przypadku tylko naglowka: */
+    udp_header->uh_dport            =       htons(atoi(argv[2]));
     udp_header->uh_ulen             =       htons(sizeof(struct udphdr));
 
-    /************************************/
-    /* Wypelnienie pol pseudo-naglowka: */
-    /************************************/
 
-
-    /* Docelowy adres IP: */
-    pseudo_header->ip_dst.s_addr    =      ((struct sockaddr_in*)rp->ai_addr)->sin_addr.s_addr;;
-    /* Pole wyzerowane: */
+    pseudo_header->ip_dst.s_addr     =       ((struct sockaddr_in*)rp->ai_addr)->sin_addr.s_addr;
     pseudo_header->unused           =       0;
-    /* Identyfikator enkapsulowanego protokolu: */
     pseudo_header->protocol         =       IPPROTO_UDP;
-    /* Rozmiar naglowka UDP i danych: */
     pseudo_header->length           =       udp_header->uh_ulen;
-    /* Obliczenie sumy kontrolnej na podstawie naglowka UDP i pseudo-naglowka: */
     udp_header->uh_sum              =       0;
+
     checksum                        =       internet_checksum(
                                                 (unsigned short *)udp_header,
                                                 sizeof(struct udphdr)
@@ -159,16 +119,11 @@ int main(int argc, char** argv) {
 
     udp_header->uh_sum              = (checksum == 0) ? 0xffff : checksum;
 
+
     fprintf(stdout, "Sending UDP...\n");
 
-    /* Wysylanie datagramow co 1 sekunde: */
     for (;;) {
 
-        /*
-         * Prosze zauwazyc, ze pseudo-naglowek nie jest wysylany
-         * (ale jest umieszczony w buforze za naglowkiem UDP dla wygodnego
-         * obliczania sumy kontrolnej):
-         */
         retval = sendto(
                      sockfd,
                      datagram, sizeof(struct udphdr),
@@ -178,6 +133,8 @@ int main(int argc, char** argv) {
 
         if (retval == -1) {
             perror("sendto()");
+        }else{
+            printf("Send UDP\n");
         }
 
         sleep(1);
